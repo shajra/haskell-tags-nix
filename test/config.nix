010e160ef7489ae91990eaf0726266b5c00b1dfa
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-09-27 20:03:53 -0500

    upgrade Hackage index (didn't change anything)

--- test/config.nix
+++ test/config.nix
@@ -2,7 +2,7 @@
     ghcVersion = "ghc884";
     haskell-nix.nixpkgs-pin = "nixpkgs-2003";
     haskell-nix.hackage.index = {
-        state = "2020-08-08T00:00:00Z";
-        sha256 = "0ikr39gh3l4r4d26227p69akg78ckml464jcz0p0c257ivbyzppw";
+        state = "2020-09-27T00:00:00Z";
+        sha256 = "0f1llfnmbsd5x1c45imc7sx0hj03j6j3g1a23m9b6f74barx4pw2";
     };
 }
