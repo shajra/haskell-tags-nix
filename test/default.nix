02569560eb590f7e63485df4372bb8006525c6ca
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-10-05 19:19:37 -0500

    clean up tests a little

--- test/default.nix
+++ test/default.nix
@@ -5,6 +5,8 @@
 
 let
 
+    lib = nixpkgs.lib;
+
     cleanSrc = nixpkgs.lib.sourceFilesBySuffices;
 
     haskell-nix =
@@ -28,63 +30,95 @@ let
         index-sha256 = config.haskell-nix.hackage.index.sha256;
     }).nix-haskell-tags-example;
 
-    tags-run = (import ../nix {}).run-static;
+    tagsMake.common = (import ../nix {}).run-static;
 
-    tags-make.nixpkgs = args: tags-run ({
+    tagsMake.nixpkgs = args: tagsMake.common ({
         haskellNix = false;
         nixFile = import ./.;
         attrPaths = [ "build.nixpkgs" ];
     } // args);
 
-    tags-make.haskell-nix = args: tags-run ({
+    tagsMake.haskell-nix = args: tagsMake.common ({
         haskellNix = true;
         nixFile = import ./.;
         attrPaths = [ "build.haskell-nix" ];
     } // args);
 
-    test = buildType: includeGhc: includeTargets: emacs:
-        let buildTags =
-                if buildType == "nixpkgs"
-                then tags-make.nixpkgs
-                else tags-make.haskell-nix;
-            includeGhcStr = if includeGhc then "ghc" else "noghc";
-            includeTargetStr = if includeTargets then "target" else "notarget";
-            emacsStr = if emacs then "emacs" else "vim";
-            tags = buildTags { inherit includeGhc includeTargets emacs; };
+    option.buildType.np.tagsMake = tagsMake.nixpkgs;
+    option.buildType.np.name = "nixpkgs";
+    option.buildType.hn.tagsMake = tagsMake.haskell-nix;
+    option.buildType.hn.name = "haskellnix";
+    option.ghcTags.includeGhc.value = true;
+    option.ghcTags.includeGhc.name = "includeGhc";
+    option.ghcTags.excludeGhc.value = false;
+    option.ghcTags.excludeGhc.name = "excludeGhc";
+    option.targetTags.includeTargets.value = true;
+    option.targetTags.includeTargets.name = "includeTargets";
+    option.targetTags.excludeTargets.value = false;
+    option.targetTags.excludeTargets.name = "excludeTargets";
+    option.format.etags.value = true;
+    option.format.etags.name = "etags";
+    option.format.ctags.value = false;
+    option.format.ctags.name = "ctags";
+
+    testName = buildType: includeGhc: includeTargets: format:
+        lib.concatStringsSep "-" [
+            "test"
+            buildType.name
+            includeGhc.name
+            includeTargets.name
+            format.name
+        ];
+
+    tag = buildType: includeGhc: includeTargets: format:
+        buildType.tagsMake {
+            includeGhc = includeGhc.value;
+            includeTargets = includeTargets.value;
+            emacs = format.value;
+        };
+
+    test = buildType: includeGhc: includeTargets: format:
+        let tags = buildType.tagsMake {
+                includeGhc = includeGhc.value;
+                includeTargets = includeTargets.value;
+                emacs = format.value;
+            };
         in tags.overrideAttrs (old: {
-            name = "nix-haskell-tags-test-${buildType}-${emacsStr}-${includeGhcStr}-${includeTargetStr}";
+            name = testName buildType includeGhc includeTargets format;
             phases = old.phases ++ ["checkPhase"];
             src = cleanSrc ./. [".bats"];
             nativeBuildInputs = with nixpkgs;
                 old.nativeBuildInputs ++ [ bats gnugrep ];
             doCheck = true;
-            BUILD_TYPE =
-                if buildType == "nixpkgs" then "Nixpkgs" else "Haskell.nix";
-            EDITOR= if includeGhc then "true" else "false";
-            INCLUDE_GHC= if includeGhc then "true" else "false";
-            INCLUDE_TARGET= if includeTargets then "true" else "false";
-            EMACS = emacsStr;
-            checkPhase = ''
-                bats $src/test-tags.bats
-            '';
+            BUILD_NAME = buildType.name;
+            INCLUDE_GHC_VALUE = includeGhc.value;
+            INCLUDE_GHC_NAME = includeGhc.name;
+            INCLUDE_TARGETS_VALUE = includeTargets.value;
+            INCLUDE_TARGETS_NAME = includeTargets.name;
+            FORMAT_NAME = format.name;
+            checkPhase = ''bats $src/test-tags.bats'';
         });
 
-in {
-    inherit build;
-    test-nixpkgs-vim-f-f = test "nixpkgs" false false false;
-    test-nixpkgs-vim-f-t = test "nixpkgs" false false true;
-    test-nixpkgs-vim-t-f = test "nixpkgs" false true false;
-    test-nixpkgs-vim-t-t = test "nixpkgs" false true true;
-    test-haskellnix-vim-f-f = test "haskellnix" false false false;
-    test-haskellnix-vim-f-t = test "haskellnix" false false true;
-    test-haskellnix-vim-t-f = test "haskellnix" false true false;
-    test-haskellnix-vim-t-t = test "haskellnix" false true true;
-    test-nixpkgs-emacs-f-f = test "nixpkgs" true false false;
-    test-nixpkgs-emacs-f-t = test "nixpkgs" true false true;
-    test-nixpkgs-emacs-t-f = test "nixpkgs" true true false;
-    test-nixpkgs-emacs-t-t = test "nixpkgs" true true true;
-    test-haskellnix-emacs-f-f = test "haskellnix" true false false;
-    test-haskellnix-emacs-f-t = test "haskellnix" true false true;
-    test-haskellnix-emacs-t-f = test "haskellnix" true true false;
-    test-haskellnix-emacs-t-t = test "haskellnix" true true true;
-}
+    testMake = buildType: includeGhc: includeTargets: format:
+        let key = testName buildType includeGhc includeTargets format;
+            val = test buildType includeGhc includeTargets format;
+        in { "${key}" = val; };
+
+in with option; with buildType; with ghcTags; with targetTags; with format;
+    { inherit build; }
+    // testMake np includeGhc includeTargets ctags
+    // testMake np includeGhc includeTargets etags
+    // testMake np includeGhc excludeTargets ctags
+    // testMake np includeGhc excludeTargets etags
+    // testMake np excludeGhc includeTargets ctags
+    // testMake np excludeGhc includeTargets etags
+    // testMake np excludeGhc excludeTargets ctags
+    // testMake np excludeGhc excludeTargets etags
+    // testMake hn includeGhc includeTargets ctags
+    // testMake hn includeGhc includeTargets etags
+    // testMake hn includeGhc excludeTargets ctags
+    // testMake hn includeGhc excludeTargets etags
+    // testMake hn excludeGhc includeTargets ctags
+    // testMake hn excludeGhc includeTargets etags
+    // testMake hn excludeGhc excludeTargets ctags
+    // testMake hn excludeGhc excludeTargets etags
--- test/test-tags.bats
+++ test/test-tags.bats
@@ -1,23 +1,25 @@
-@test "$BUILD_TYPE: $EMACS: GHC=$INCLUDE_GHC: TARGET=$INCLUDE_TARGET: includes/excludes ghc" {
-    if "$INCLUDE_GHC"
+HEADER="$BUILD_NAME: $INCLUDE_GHC_NAME: $INCLUDE_TARGETS_NAME: $FORMAT_NAME"
+
+@test "$HEADER: includes/excludes ghc" {
+    if "$INCLUDE_GHC_VALUE"
     then grep --extended-regexp '/nix/store/.*-ghc-' "$out"
     else grep --invert-match --extended-regexp '/nix/store/.*-ghc-' "$out"
     fi
 }
 
-@test "$BUILD_TYPE: $EMACS: GHC=$INCLUDE_GHC: TARGET=$INCLUDE_TARGET: includes putStrLn" {
-    if "$INCLUDE_GHC"
+@test "$HEADER: includes/excludes putStrLn" {
+    if "$INCLUDE_GHC_VALUE"
     then grep --extended-regexp '\bputStrLn\b' "$out"
     else grep --invert-match --extended-regexp '\bputStrLn\b' "$out"
     fi
 }
 
-@test "$BUILD_TYPE: $EMACS: GHC=$INCLUDE_GHC: TARGET=$INCLUDE_TARGET: includes void" {
+@test "$HEADER: includes void" {
     grep '/nix/store/.*-void-' "$out"
 }
 
-@test "$BUILD_TYPE: $EMACS: GHC=$INCLUDE_GHC: TARGET=$INCLUDE_TARGET: includes/excludes example project" {
-    if "$INCLUDE_TARGET"
+@test "$HEADER: includes/excludes example project" {
+    if "$INCLUDE_TARGETS_VALUE"
     then grep --extended-regexp '\bexampleMsg\b' "$out"
     else grep --invert-match --extended-regexp '\bexampleMsg\b' "$out"
     fi
