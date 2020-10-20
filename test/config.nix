d14711bc93b5cf40fa4c8ea1cfe700e1e8d870f8
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-10-19 22:39:37 -0500

    upgrade Hackage index

--- test/config.nix
+++ test/config.nix
@@ -2,8 +2,8 @@
     ghcVersion = "ghc884";
     haskell-nix.nixpkgs-pin = "nixpkgs-2003";
     haskell-nix.hackage.index = {
-        state = "2020-10-05T00:00:00Z";
-        sha256 = "1sazxknvajhyb6v8xhmi0ajgnz5jz300iww75gh5x8xnaf7lhfvw";
+        state = "2020-10-19T00:00:00Z";
+        sha256 = "03a98qiqr53csy59pwgbfvnzg90ikaiz9gvxv6s9aw95f990z9mb";
 
     };
 }
