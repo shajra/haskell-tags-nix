1adbed902b6eb647be0c42a31b07cec843530e38
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-11-13 20:39:21 -0600

    upgrade dependencies and documentation

--- test/config.nix
+++ test/config.nix
@@ -1,9 +1,8 @@
 {
     ghcVersion = "ghc884";
-    haskell-nix.nixpkgs-pin = "nixpkgs-2003";
+    haskell-nix.nixpkgs-pin = "nixpkgs-2009";
     haskell-nix.hackage.index = {
-        state = "2020-10-19T00:00:00Z";
-        sha256 = "03a98qiqr53csy59pwgbfvnzg90ikaiz9gvxv6s9aw95f990z9mb";
-
+        state = "2020-11-14T00:00:00Z";
+        sha256 = "f7797686b1d20e7fecfc8691b08047245e5e2b51d75f9191d8b841b2a1f43fd3";
     };
 }
