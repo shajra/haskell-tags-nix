cae4f54c74bd9398f4b606c1cfef6cb68f9d2fff
Sukant Hajra <rrudbskr5g@snkmail.com>
2021-02-21 10:43:15 -0600

    update dependencies and regenerate documentation

--- test/config.nix
+++ test/config.nix
@@ -2,8 +2,8 @@
     haskell-nix.checkMaterialization = false;
     # DESIGN: https://github.com/input-output-hk/hackage.nix/blob/master/index-state-hashes.nix
     haskell-nix.hackage.index = {
-        state = "2021-02-14T00:00:00Z";
-        sha256 = "db71cd0dfe6bb7957b52b50ab8cb268f9debe65a37a1983451ec610ae143c59b";
+        state = "2021-02-21T00:00:00Z";
+        sha256 = "9bdefd7052080d8989f398de529973e993d2911993b6c2fb9cfe801dad47879d";
     };
     haskell-nix.nixpkgs-pin = "nixpkgs-2009";
     haskell-nix.ghcVersion = "ghc8104";
